633fa5b4931d7f0bc2ee756057d95b1f1c0c6d94
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-27 15:44:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
-  attr_accessor :name, :artist_name	  attr_accessor :name, :artist_name
-  @@all = []	  @@all = []
-@@ -10,4 +12,65 @@ def save
-    self.class.all << self	    self.class.all << self
-  end	  end
+  attr_accessor :name, :artist_name
+  @@all = []
+ @@ def save
+  self.class.all << self
+ end
 
 
   def self.create
